a2cd7e6235c7fefb453f85dd70d84db427d804d5
shara.khanal <sharad.khanal@sbsolutionsnepal.com>
2024-02-08 21:15:05 +0545

    add adminDashboard and side bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import {SignupComponent} from "./pages/signup/signup.component";
 import {LoginComponent} from "./pages/login/login.component";
 import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {AdminDashboardComponent} from "./pages/admin/admin-dashboard/admin-dashboard.component";
+import {UserDashboardComponent} from "./pages/user/user-dashboard/user-dashboard.component";
+import {ProfileComponent} from "./pages/profile/profile.component";
+import {WelcomeComponent} from "./pages/admin/welcome/welcome.component";
 
 const routes: Routes = [
   {path: '',
@@ -21,6 +25,25 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomePageComponent,
+    pathMatch: "full"
+  },
+  {
+    path: 'admin-dashboard',
+    component: AdminDashboardComponent,
+    children: [
+      {
+        path: '',
+        component: WelcomeComponent
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent
+      }
+    ]
+  },
+  {
+    path: 'user-dashboard',
+    component: UserDashboardComponent,
     pathMatch: "full"
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,13 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { MatCardModule} from "@angular/material/card";
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from "@angular/material/icon";
+import { AdminDashboardComponent } from './pages/admin/admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import {MatListModule} from '@angular/material/list';
+import {SideBarComponent} from "./pages/admin/side-bar/side-bar.component";
+import { WelcomeComponent } from './pages/admin/welcome/welcome.component';
+
 
 @NgModule({
   declarations: [
@@ -25,7 +32,12 @@ import {MatIconModule} from "@angular/material/icon";
     FooterComponent,
     SignupComponent,
     LoginComponent,
-    HomePageComponent
+    HomePageComponent,
+    AdminDashboardComponent,
+    UserDashboardComponent,
+    ProfileComponent,
+    SideBarComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
@@ -38,7 +50,8 @@ import {MatIconModule} from "@angular/material/icon";
     HttpClientModule,
     MatCardModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
